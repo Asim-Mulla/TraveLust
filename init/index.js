9c45a604e2290f5ef6d8a6614152326d9e591be8
Asim Mulla <asimmulla2004@gmail.com>
2024-09-24 22:56:40 +0530

    Add Project Files

---
+++ init/index.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Listing = require("../models/listing.js");
+const initData = require("./data.js");
+
+main()
+  .then((res) => {
+    console.log("connection created");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+async function main() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/traveLust");
+}
+
+const initDB = async () => {
+  await Listing.deleteMany({});
+  initData.data = initData.data.map((obj) => ({
+    ...obj,
+    owner: "66d9751c8e8f7e6646ca6732",
+  }));
+  await Listing.insertMany(initData.data);
+  console.log("data inserted");
+};
+
+initDB();
---
+++ models/listing.js
@@ -0,0 +1,76 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const Review = require("./review.js");
+const { required } = require("joi");
+
+const listingSchema = Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  image: {
+    url: String,
+    filename: String,
+  },
+  price: {
+    type: Number,
+  },
+  location: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+  reviews: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Review",
+    },
+  ],
+  owner: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  geometry: {
+    type: {
+      type: String,
+      enum: ["Point"],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+  category: {
+    type: String,
+    enum: [
+      "Pools",
+      "Trecking",
+      "Beaches",
+      "Rooms",
+      "Castles",
+      "Boats",
+      "Arctic",
+      "Iconic Cities",
+      "Mountains",
+      "Parties",
+      "Sports",
+      "Gyms",
+      "Camping",
+      "Snowboarding",
+      "Birthday",
+    ],
+  },
+});
+
+listingSchema.post("findOneAndDelete", async (listing) => {
+  if (listing) {
+    await Review.deleteMany({ _id: { $in: listing.reviews } });
+  }
+});
+
+module.exports = mongoose.model("Listing", listingSchema);
